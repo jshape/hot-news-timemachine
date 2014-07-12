71106d8e77819cbd6bf53876a66eec7d51f8fdea
Kenni Bawden <kenni.bawden@gmail.com>
2014-07-12 10:31:31 +1000

    Outline of functionality
    Currently replaces content with example content.
    
    TODO
    Get article keywords from AlchemyAPI
    Fetch the old article from Trove.

--- timemachine.js
+++ timemachine.js
@@ -1,1 +1,33 @@
-$('#story').html("Old news");
+function getArticleKeywords(website) {
+    var keywords = ["example", "story"];
+    return keywords;
+};
+
+function getOldArticle(keywords) {
+    var article = {
+        title: "It's the 1900s!",
+        body: "An example article from the 1900s",
+    };
+
+    return article;
+};
+
+function replaceArticle(oldArticle, website) {
+    var title, body;
+    if (website === "http://news.com.au") {
+        title = ".story-headline h1.heading";
+        body = ".story-body";
+    }
+
+    $(title).html(oldArticle.title);
+    $(body).html(oldArticle.body);
+}
+
+function timemachine() {
+    var website = "http://news.com.au";
+    var keywords = getArticleKeywords(website);
+    var oldArticle = getOldArticle(keywords);
+    replaceArticle(oldArticle, website);
+};
+
+timemachine();
